c85c981bcebdaa0c86f3165e0bd70bae6c903343
ramiroSicilia <moto2.belgrano@gmail.com>
2025-10-24 18:02:42 -0300

    Agregada página de capítulos y arreglo de estilos

--- src/Components/LibrosCard.jsx
+++ src/Components/LibrosCard.jsx
@@ -42,14 +42,14 @@ export default function LibrosCard({ libro }) {
             display: "flex",
             flexDirection: "column",
             alignItems: "flex-start",
-            gap:"1rem",
+            gap:".8rem",
             padding:".4rem 0px"
           }}
         >
-          <p style={{ fontSize: "13px", color: "#444", margin: "1px 0" }}>
+          <p style={{ fontSize: "13px", color: "#444", margin: ".8px 0" }}>
             <strong>Categoría:</strong> {libro.categoria}
           </p>
-          <p style={{ fontSize: "13px", color: "#444", margin: "1px 0" }}>
+          <p style={{ fontSize: "13px", color: "#444", margin: ".8px 0" }}>
             <strong>Género:</strong> {libro.genero} 
           </p> 
           <p style={{ fontSize: "14px", color: "#666" }}>{libro.descripcion}</p>
