bc03a35f1e4859c22f865bd5d6af7bca409c904f
ramiroSicilia <moto2.belgrano@gmail.com>
2025-11-02 15:32:18 -0300

    Agregada página de capítulos y arreglo de estilos

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -40,7 +40,7 @@ export default function Footer() {
 
         {/* Facebook */}
         <a
-         href="https://www.facebook.com/profile.php?id=61579275134722/posts"
+         href="https://www.facebook.com"
           target="_blank"
           rel="noopener noreferrer"
           className="footer-icon footer-facebook"
